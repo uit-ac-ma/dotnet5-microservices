3260f4bd895ccc5ca50d2fc8cb7fc50e01f30df3
bmassioui <bouchaib.massioui@gmail.com>
2022-01-09 00:13:18 +0100

    #19 Data Layer setup

--- src/Services/Catalog/Catalog.API/Entities/Product.cs
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -8,6 +8,8 @@ namespace Catalog.API.Entities
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
+
+        [BsonElement("Name")]
         public string Name { get; set; }
         public string Category { get; set; }
         public string Summary { get; set; }
